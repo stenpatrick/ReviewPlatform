23e884c0bda424d9d821fcf1a5f56f49918a7b87
Opilane TTHK <opilane@tthk.ee>
2024-10-29 11:46:02 +0200

    vue

---
+++ frontend/app.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: "DoctorsTable",
+    props: {
+        items: Array
+    }
+}
+</script>
+
+<template>
+<table class="table table-striped">
+    <thead class="table-dark">
+    <tr>
+        <th>Name</th>
+        <th>Rating</th>
+    </tr>
+    </thead>
+<tbody>
+    <tr v-for="item in items">
+        <td>{{item.name}}</td>
+        <td>{{item.rating}}</td>
+    </tr>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+</tbody>
+</table>
+</template>

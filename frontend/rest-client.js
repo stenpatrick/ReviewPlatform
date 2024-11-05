b9ec2d4651b8d295b73447f8ca9f09e5d881c207
Opilane TTHK <opilane@tthk.ee>
2024-11-05 15:34:29 +0200

    frontend backend working

---
+++ frontend/rest-client.js
@@ -0,0 +1,18 @@
+    const vue = Vue.createApp({
+        data() {
+            return { 
+                doctorInModal: {name: null},
+                doctors: []
+            }
+        },
+        async created() {
+            this.doctors = await (await fetch('http://127.0.0.1:8080/doctors')).json();
+        },
+        methods: {
+            getDoctor: async function (id) {
+                this.doctorInModal = await (await fetch ('http://127.0.0.1:8080/doctors/${id}')).json()
+                let doctorInModal = new bootstrap.Modal(document.getElementById('doctorInfoModal'), {})
+                doctorInfoModal.show()
+            }
+        }
+    }).mount('#app')

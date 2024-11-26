55078b4c0711ce10d81d0e5b3c39303848bc6249
opilane <opilane@HP-CZC321706M>
2024-11-26 12:31:12 +0200

    Update rest-client.js

--- frontend/rest-client.js
+++ frontend/rest-client.js
@@ -13,6 +13,12 @@
                 this.doctorInModal = await (await fetch ('http://127.0.0.1:8080/doctors/${id}')).json()
                 let doctorInModal = new bootstrap.Modal(document.getElementById('doctorInfoModal'), {})
                 doctorInfoModal.show()
-            }
+            },
+            addDoctor: async function (name, rating, contact) {
+                this.doctorInModal = await (await fetch ('http://127.0.0.1:8080/doctors/${id}')).json()
+                let doctorInModal = new bootstrap.Modal(document.getElementById('doctorInfoModal'), {})
+                doctorInfoModal.show()
+            },
+
         }
     }).mount('#app')
